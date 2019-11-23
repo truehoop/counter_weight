a9da0a63bfe3e35d027eb5a2af53ac9beff8ec7f
martionlee <martionlee@gmail.com>
2019-11-23 14:15:57 +0900

    Commit to Database after crawling news

--- read_news_body.py
+++ read_news_body.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import traceback
+import copy
 
 from bs4 import BeautifulSoup
 import asyncio
@@ -13,7 +14,7 @@ from utils.time import now_ms_ts
 lines = '''byLine: 
 categoryCodes: []
 dateCodes: []
-endDate: 2019-11-12
+endDate: 2019-11-20
 incidentCodes: []
 indexName: news
 isNotTmUsable: false
@@ -47,7 +48,7 @@ data = {
 "byLine": "",
 "categoryCodes": [],
 "dateCodes": [],
-"endDate": "2019-11-16",
+"endDate": "2019-11-20",
 "incidentCodes": [],
 "indexName": "news",
 "isNotTmUsable": 'false',
@@ -59,13 +60,13 @@ data = {
 "resultNumber": 10,
 "searchFilterType": "1",
 # "searchKey": "삼성 최순실",
-"searchKey": "(삼성 OR  신세계 OR  현대 OR  SK OR LG)",
-# "searchKeys": [{}],
-"searchKeys": [{"orKeywords": ["삼성, 신세계, 현대, SK,LG"]}],
-"searchScopeType": "1",
+"searchKey": "(삼성 OR 신세계 OR 현대 OR SK OR LG OR 롯데 OR 포스코 OR 한화 OR GS OR 농협 OR KT) AND (가계빈곤 OR 경제노출 OR 경제충격 OR 근로빈곤층 OR 기아 OR 기초생활 OR 기초생활보장 OR 남성빈곤 OR 노인빈곤 OR 노인일자리 OR 미달가구 OR 보험 OR 빈곤 OR 빈곤인구 OR 빈곤층 OR 사각지대 OR 사회노출 OR 사회보장 OR 사회보장제도 OR 사회서비스 OR 사회안전망 OR 사회충격 OR 생계 OR 성별빈곤 OR 소득 OR 실업급여 OR 여성빈곤 OR 영양실조 OR 의료 OR 이주민빈곤 OR 장애빈곤 OR 재난 OR 재난노출 OR 주거 OR 청년빈곤 OR 청소년빈곤 OR 최저주거 OR 취약계층 OR 환경노출 OR 환경충격)",
+"searchKeys": [{}],
+# "searchKeys": [{"orKeywords": ["삼성, 신세계, 현대, SK,LG"]}],
+"searchScopeType": "2",
 "searchSortType": "date",
 "sortMethod": "date",
-"startDate": "2019-10-16",
+"startDate": "2018-11-20",
 "startNo": 1,
 "topicOrigin": ""
 }
@@ -92,14 +93,16 @@ count = 0
 async def fetch_post(session, url, body):
     try:
         async with session.post(url, json=body, headers=headers) as response:
-            return await response.json()
+            res = await response.json()
+            return res
     except Exception as e:
         print('e={}, trace={}'.format(repr(e), traceback.format_exc()))
 
 async def fetch_get(session, url, result):
     try:
         async with session.post(url, headers=headers) as response:
-            return await response.json()
+            res = await response.json()
+            return res
     except Exception as e:
         print('e={}, trace={}'.format(repr(e), traceback.format_exc()))
         print(result)
@@ -134,23 +137,22 @@ async def post_news_list():
         print('totalCount: ', first_call['totalCount'])
         for index in range(1, int((first_call['totalCount']+9)/10)):
             data['startNo'] = index
-            tasks.append(fetch_post(session, result_url, data))
+            temp_data = copy.copy(data)
+            tasks.append(fetch_post(session, result_url, temp_data))
         
         before_read_news_detail = now_ms_ts()
         news_list = await asyncio.gather(*tasks)
         print('뉴스 리스트 호출 시 걸린 시간 :', now_ms_ts() - before_read_news_detail)
         detail_tasks = []
         for news in news_list:
-            # print(news)
-            # print(news['resultList'])
             detail_list = read_news_detail(session, news)
             detail_tasks.extend(detail_list)
         
         news_detail_list = await asyncio.gather(*detail_tasks)
+        
         print('전체 호출 시 걸린 시간 :', now_ms_ts() - before_read_news_detail)
 
-        for news_detail in news_detail_list:
-            pass
+        return news_detail_list
             # print(news_detail['detail']['TITLE'])
     # response = requests.post(result_url, data=body, headers=headers)
     # print(response)
@@ -160,19 +162,10 @@ async def post_news_list():
     # print(res['totalCount'])
     # print(res['resultList'])
 
-async def main():
-    print('Hello ...')
-    await asyncio.sleep(1)
-    await post_news_list()
-    print('... World!')
-    
-
-asyncio.run(main())
-
-def parsing(response):
-    html = response.text
-    soup = bs4(html, 'html.parser')
-    print(response.text)
+# def parsing(response):
+#     html = response.text
+#     soup = bs4(html, 'html.parser')
+#     print(response.text)
 
 # for tag in soup.select('.resultList li h3'):
 #     doc_id = tag['id'].replace('news_', '')
@@ -207,49 +200,65 @@ print(f'Now you gonna Connect to {db_url}')
 cursor = database.cursor()
 
 # Create the INSERT INTO sql query
-query = """INSERT INTO news_data_all (id, news_date, media_name, writer, title, key1, key2, key3, acident1, acident2, acident3, `character`, location, agency, keyword, keyword_export, sentence, url, exception) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE duplication = VALUES(duplication) + 1"""
+query = """INSERT INTO manjum.news_data_all (id, news_date, media_name, writer, title, key1, key2, key3, acident1, acident2, acident3, `character`, location, agency, keyword, keyword_export, url, exception, body) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE duplication = VALUES(duplication) + 1"""
 
-# book = xlrd.open_workbook(file_name)
-# sheet = book.sheet_by_name("sheet")
-# cursor = database.cursor()
+def insert_news_to_db(news_detail_list):
+    before_query = now_ms_ts()
 
-# max_length = 0
-# for r in range(1, sheet.nrows):
-#     id  = sheet.cell(r,0).value
-#     news_date   = sheet.cell(r,1).value
-#     media_name  = sheet.cell(r,2).value
-#     writer  = sheet.cell(r,3).value
-#     title   = sheet.cell(r,4).value
-#     key1    = sheet.cell(r,5).value
-#     key2    = sheet.cell(r,6).value
-#     key3    = sheet.cell(r,7).value
-#     acident1    = sheet.cell(r,8).value
-#     acident2    = sheet.cell(r,9).value
-#     acident3    = sheet.cell(r,10).value
-#     character   = sheet.cell(r,11).value
-#     location    = sheet.cell(r,12).value
-#     agency  = sheet.cell(r,13).value
-#     keyword = sheet.cell(r,14).value
-#     keyword_export  = sheet.cell(r,15).value
-#     sentence    = sheet.cell(r,16).value
-#     url = sheet.cell(r,17).value
-#     exception    = sheet.cell(r,18).value
+    print(len(news_detail_list))
 
-#     # Assign values from each row
-#     values = (id, news_date, media_name, writer, title, key1, key2, key3, acident1, acident2, acident3, character, location, agency, keyword, keyword_export, sentence, url, exception)
+    for detail in news_detail_list:
+        # print(detail)
+        detail_data = detail['detail']
+        # print(detail_data['TITLE'])
+        id          = detail_data['NEWS_ID']
+        news_date   = detail_data['DATE']
+        media_name  = detail_data['PROVIDER']
+        writer      = detail_data['BYLINE']
+        title       = detail_data['TITLE']
+        key         = detail_data['CATEGORY'].split('|')
+        key1        = key[0] if len(key) > 0 else ''
+        key2        = key[1] if len(key) > 1 else ''
+        key3        = key[2] if len(key) > 2 else ''
+        acident     = detail_data['CATEGORY_INCIDENT'].split('|')
+        acident1    = acident[0] if len(acident) > 0 else ''
+        acident2    = acident[1] if len(acident) > 1 else ''
+        acident3    = acident[2] if len(acident) > 2 else ''
+        character   = detail_data['TMS_NE_PERSON']
+        location    = detail_data['TMS_NE_LOCATION']
+        agency      = detail_data['TMS_NE_ORGANIZATION']
+        keyword     = detail_data['TMS_RAW_STREAM']
+        keyword_export= detail_data['TMS_NE_STREAM']
+        url         = detail_data['PROVIDER_LINK_PAGE']
+        exception   = ''
+        body        = detail_data['CONTENT']
+        
+        values = (id, news_date, media_name, writer, title, key1, key2, key3, acident1, acident2, acident3, character, location, agency, keyword, keyword_export, url, exception, body)
+        
+        if title == '' or writer == '' or id == '' or media_name == '':
+            print(values)
+        try:
+            cursor.execute(query, values)
+        except Exception as e:
+            print('e={}, trace={}'.format(repr(e), traceback.format_exc()))
 
-#     length = len(keyword)
-#     if max_length < length:
-#         max_length = length
-#     # Execute sql Query
-#     cursor.execute(query, values)
 
-# print(max_length)
-# # Close the cursor
-# cursor.close()
-# # Commit the transaction
-# database.commit()
-# print(file_name, 'Done !')
+    # Close the cursor
+    cursor.close()
+    # Commit the transaction
+    database.commit()
 
-# # Close the database connection
-# database.close()
+    # Close the database connection
+    database.close()
+
+    print('db insert time :', now_ms_ts() - before_query)
+
+async def print_when_done(tasks):
+    for res in asyncio.as_completed(tasks):
+        news_detail_list = await res
+        insert_news_to_db(news_detail_list)
+
+coros = [post_news_list()]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(print_when_done(coros))
+loop.close()
